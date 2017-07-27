3d131d3929da0fd13f8891badd860f8ba8431231
Christian Hancke <c.hancke@live.de>
2017-07-27 11:16:58 +0200

    Update quelle.cpp

--- quelle.cpp
+++ quelle.cpp
@@ -4,15 +4,16 @@ using namespace std;
 
 int main ()
 {
+    // Die Variablen
     
-    float fahrenheit = 50;
+    float fahrenheit = 50; // Den den umzurechnenden Wert eintragen
     float celsius = 0;
     
     // Die formel
     
     celsius = ((fahrenheit - 32) * 5 ) / 9;
     
-    cout << celsius;
+    cout << fahrenheit << " Grad Fahrenheit entspricht " << celsius << " Grad Celsius" << endl;
     
     return 0;
 }
